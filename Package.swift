fd0408fc961c7ffa8f5d02776d55c2a79912477e
reuschj <reuschj@gmail.com>
2021-08-23 23:16:57 -0500

    added missing product

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
         .library(
             name: "BirdrAPIClient",
             targets: ["BirdrAPIClient"]),
+        .library(
+            name: "BirdrAPIFoundation",
+            targets: ["BirdrAPIFoundation"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -31,7 +34,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "BirdrAPIFoundation", dependencies: []),
+        .target(
+            name: "BirdrAPIFoundation",
+            dependencies: []
+        ),
         .target(
             name: "BirdrAPIClient",
             dependencies: [
