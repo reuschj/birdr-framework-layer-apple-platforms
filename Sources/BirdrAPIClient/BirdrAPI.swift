a9cd6a399843407cf03c1504a40e5621b033df93
reuschj <reuschj@gmail.com>
2021-08-24 12:35:07 -0500

    feat/minor updates

--- Sources/BirdrAPIClient/BirdrAPI.swift
+++ Sources/BirdrAPIClient/BirdrAPI.swift
@@ -1,3 +1,25 @@
 import Foundation
 
+public struct BirdrAPI {
 
+    // APIs ----------------------------- /
+    public var image: BirdrImageAPI
+    public var spotting: BirdrSpottingAPI
+    // --------------------------------- /
+    
+    public var context: BirdrAPIContext {
+        didSet {
+            // If we change the main context, change all the contexts below as well
+            self.image.context = self.context
+            self.spotting.context = self.context
+        }
+    }
+    
+    public init(
+        context: BirdrAPIContext = .init()
+    ) {
+        self.context = context
+        self.image = .init(context: context)
+        self.spotting = .init(context: context)
+    }
+}
